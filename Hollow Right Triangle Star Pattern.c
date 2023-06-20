976780f5ec83fc919224da63e8d68fcd32925bf1
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2023-06-20 21:00:11 +0530

    Hollow Right Triangle Star Pattern.c

--- Hollow Right Triangle Star Pattern.c
+++ Hollow Right Triangle Star Pattern.c
@@ -1,5 +1,9 @@
 #include <stdio.h>  
-      
+
+//this code is for online & some ofline compilers. 
+// If needed, Kindly add "<conio.h>" & other required header files.
+
+
     int main()  
     {  
         int n;  
